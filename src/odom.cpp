8230d35f06c975e262f0babc2e830fe7a3553fa1
pattylo <19451945as@gmail.com>
2025-05-05 18:24:21 +0800

    have pose now

--- src/odom.cpp
+++ src/odom.cpp
@@ -15,9 +15,14 @@
 
 using namespace cv;
 using namespace std;
-static ros::Subscriber fisheye_sub;
+static ros::Subscriber watereye_sub;
 static cv::Mat img;
 static cv_bridge::CvImageConstPtr imageptr;
+static cv::Mat camMat = cv::Mat::eye(3, 3, CV_64F);
+static std::vector<cv::Point3d> pts_3d_final;
+static std::vector<cv::Point2d> pts_2d_final;
+static cv::Vec3d rvec, tvec;
+static cv::Mat distCoeffs = cv::Mat::zeros(8, 1, CV_64F);
 
 void image_callback(const sensor_msgs::Image::ConstPtr& imagemsg)
 {
@@ -53,34 +58,35 @@ void image_callback(const sensor_msgs::Image::ConstPtr& imagemsg)
 
     std::cout << zarray_size(detections) << std::endl;
 
-    // // Print and draw detections
-    // for (int i = 0; i < zarray_size(detections); i++) {
-    //     apriltag_detection_t* det;
-    //     zarray_get(detections, i, &det);
+    pts_2d_final.clear();
 
-    //     ROS_INFO("Detected tag ID: %d", det->id);
+    for (int i = 0; i < zarray_size(detections); i++)
+    {
+        apriltag_detection_t* det;
+        zarray_get(detections, i, &det);
 
-    //     // Draw outline
-    //     for (int j = 0; j < 4; j++) {
-    //         cv::line(img,
-    //             cv::Point(det->p[j][0], det->p[j][1]),
-    //             cv::Point(det->p[(j+1)%4][0], det->p[(j+1)%4][1]),
-    //             cv::Scalar(0, 255, 0), 2);
-    //     }
+        for (int j = 0; j < 4; j++)
+        {
+            cv::circle(img, cv::Point(det->p[j][0], det->p[j][1]), 5, cv::Scalar(0, 255, 0), -1);
+            // std::cout<<cv::Point(det->p[j][0], det->p[j][1])<<std::endl<<std::endl;
+            pts_2d_final.emplace_back(cv::Point2d(det->p[j][0], det->p[j][1]));
+        }
+        cv::circle(img, cv::Point(det->c[0], det->c[1]), 5, cv::Scalar(0, 0, 255), -1);
+    }
 
-    //     // Draw tag center
-    //     cv::circle(img, cv::Point(det->c[0], det->c[1]), 5, cv::Scalar(0, 0, 255), -1);
-
-    //     // Put tag ID
-    //     cv::putText(img, to_string(det->id), cv::Point(det->c[0], det->c[1]),
-    //                 cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 0, 0), 2);
-    // }
-
-    // // Show image
+    cv::solvePnP(pts_3d_final, pts_2d_final, camMat, distCoeffs, rvec, tvec, cv::SOLVEPNP_ITERATIVE);
+    
     // cv::imshow("AprilTag Detection", img);
-    // cv::waitKey(10);
+    // cv::waitKey(5);
 
-    // Cleanup
+    cv::Mat rmat = cv::Mat::eye(3,3,CV_64F);
+    cv::Rodrigues(rvec, rmat);
+
+    // std::cout<<"POSE HERE"<<std::endl;
+    // std::cout<<rmat<<std::endl;
+    // std::cout<<tvec<<std::endl;
+    // std::cout<<std::endl<<std::endl;
+    
     apriltag_detections_destroy(detections);
     apriltag_detector_destroy(td);
     tag36h11_destroy(tf);
@@ -91,34 +97,43 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "odom");
     ros::NodeHandle nh;
 
-    fisheye_sub = nh.subscribe<sensor_msgs::Image>
+    watereye_sub = nh.subscribe<sensor_msgs::Image>
         ("/stereo_camera/raw", 1, &image_callback);
-    
-        
-    // image_u8_t* im = image_u8_create_from_pnm("test.pnm");
-    // if (im == NULL) {
-    //     fprintf(stderr, "Failed to load pnm image.\n");
-    //     exit(1);
-    // }
-    // apriltag_detector_t *td = apriltag_detector_create();
-    // apriltag_family_t *tf = tag36h11_create();
-    // apriltag_detector_add_family(td, tf);
-    // zarray_t *detections = apriltag_detector_detect(td, im);
-    
-    // for (int i = 0; i < zarray_size(detections); i++) {
-    //     apriltag_detection_t *det;
-    //     zarray_get(detections, i, &det);
-    
-    //     // Do stuff with detections here.
-    // }
-    // // Cleanup.
-    // apriltag_detections_destroy(detections);
-    // tag36h11_destroy(tf);
-    // apriltag_detector_destroy(td);
 
+    XmlRpc::XmlRpcValue intrinsics_list;
+
+    std::cout<<1<<std::endl;
+    
+    nh.getParam("cam_intrinsics", intrinsics_list);
+    std::cout<<1<<std::endl;
+
+    for (int i = 0; i < 4; i ++)
+    {
+        std::cout<<intrinsics_list[i]<<std::endl;
+    }
+
+    std::cout<<"loop"<<std::endl;
+
+    camMat.at<float>(0, 0) = static_cast<float>(static_cast<double>(intrinsics_list[0]));
+    camMat.at<float>(0, 1) = static_cast<float>(static_cast<double>(intrinsics_list[1]));
+    camMat.at<float>(1, 1) = static_cast<float>(static_cast<double>(intrinsics_list[2]));
+    camMat.at<float>(1, 2) = static_cast<float>(static_cast<double>(intrinsics_list[3]));
+
+
+    double tag_size = 0.3;
+
+    pts_3d_final.emplace_back(cv::Point3d(-tag_size / 2,  0.0, -tag_size / 2));
+    pts_3d_final.emplace_back(cv::Point3d(tag_size / 2,  0.0, -tag_size / 2));
+    pts_3d_final.emplace_back(cv::Point3d(tag_size / 2,  0.0, tag_size / 2));
+    pts_3d_final.emplace_back(cv::Point3d(-tag_size / 2, 0.0, tag_size / 2));
+
+    std::cout<<1<<std::endl;
+
+    std::cout<<"INITIALIZATION DONE"<<std::endl;
+    
     ros::spin();
     
     return 0;
     
-    
+
 }
